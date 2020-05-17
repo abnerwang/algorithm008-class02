acedb893b92cdc7e11495583532d6b730b31f91c
abnerwang <abnerwang@yeah.net>
2020-05-17 21:59:59 +0800

    update Week_04 homework

---
+++ Week_04/153.寻找旋转排序数组中的最小值.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=153 lang=python3
+#
+# [153] 寻找旋转排序数组中的最小值
+#
+# https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (50.33%)
+# Likes:    180
+# Dislikes: 0
+# Total Accepted:    47.5K
+# Total Submissions: 93.4K
+# Testcase Example:  '[3,4,5,1,2]'
+#
+# 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+#
+# ( 例如，数组 [0,1,2,4,5,6,7]  可能变为 [4,5,6,7,0,1,2] )。
+#
+# 请找出其中最小的元素。
+#
+# 你可以假设数组中不存在重复元素。
+#
+# 示例 1:
+#
+# 输入: [3,4,5,1,2]
+# 输出: 1
+#
+# 示例 2:
+#
+# 输入: [4,5,6,7,0,1,2]
+# 输出: 0
+#
+#
+
+
+# @lc code=start
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low < high:
+            mid = (low + high) >> 1
+            if nums[mid] > nums[high]:
+                low = mid + 1
+            else:
+                high = mid
+
+        return nums[low]
+
+
+# @lc code=end
